afd8bfe24afc5856796885520d50af0ed1c6f605
Furkan <Furkan@DESKTOP-J37MVVO>
2022-09-07 17:48:28 +0300

    Add project files.

---
+++ Serialization_Library/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Runtime.Serialization; // Gerekli namespace eklenir
+using System.Runtime.Serialization.Formatters.Binary; // Binary serialization için
+
+namespace Serialization_Library
+{
+    [Serializable] // Sınıfı serileştirme için uygun hale getirir.
+    public class Person : ISerializable
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int ID { get; set; }
+
+        public Person(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context) // Kaydedilecek özellikleri alan metod
+        {
+            info.AddValue("İsim", Name);
+            info.AddValue("Soyisim", LastName);
+        }
+
+        public Person(SerializationInfo info, StreamingContext context) 
+        // Bir constructor metot yaratılarak kaydedilen değerleri tekrar sınıf içerisine atam
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            LastName = (string)info.GetValue("Soyisim", typeof(string));
+        }
+    }
+}
---
+++ Serialization_Library/SerializationLibrary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Serialization_Library
+{
+    public class SerializationLibrary
+    {
+        public SerializationLibrary()
+        {
+            #region BinarySerialization
+            
+            Person newPerson = new Person("Furkan", "Şahin");
+
+            Stream stream = File.Open("PersonData.dat", FileMode.OpenOrCreate);
+            // Stream türünde bir kayıt dosyası tanımlarız. Kayıt dosyası mevcut değilse oluştururuz
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            stream.Close();
+
+            bf.Serialize(stream, newPerson);
+
+            stream = File.Open("PersonData.dat", FileMode.Open);
+
+            newPerson = (Person)bf.Deserialize(stream);
+
+            stream.Close();
+
+            #endregion
+
+            #region XML Serialization
+            
+            XmlSerializer xs = new XmlSerializer(typeof(Person));
+
+            using (TextWriter tw = new StreamWriter(@"C:\Kullanıcılar\Furkan\Masaüstü"))
+            {
+                xs.Serialize(tw, newPerson);
+            }
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(Person));
+
+            TextReader tr = new StreamReader(@"C:\Kullanıcılar\Furkan\Masaüstü");
+
+            object obj = deserializer.Deserialize(tr);
+
+            newPerson = (Person)obj;
+
+            tr.Close();
+
+            #endregion
+
+
+        }
+    }
+}
